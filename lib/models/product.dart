c31284d74730f43af6d34f0c35e5419cd6adcb65
Ayb <aymojobe@gmail.com>
2026-08-11 21:06:30 +0100

    Product Model View

---
+++ lib/models/product.dart
@@ -0,0 +1,51 @@
+class Product {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String imageURL;
+  final String catergory;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageURL,
+    required this.catergory,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      price: json['price'],
+      imageURL: json['imageURL'],
+      catergory: json['catergory'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "name": name,
+      "description": description,
+      "price": price,
+      "imageURL": imageURL,
+      "category": catergory,
+    };
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Product && runtimeType == other.runtimeType && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+
+  @override
+  String toString() =>
+      "User(id: $id, name: $name, description: $description, price: $price)";
+}
